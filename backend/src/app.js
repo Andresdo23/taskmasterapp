f08e3af6fd4f4de375a017b93c69d17ee2609637
Andre Oliveira <andresdo026@gmail.com>
2025-03-31 02:01:43 -0300

    atualizando o backend

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const Parse = require('parse/node'); 
 require('dotenv').config();
-const router = require('../router');
+const router = require('./router');
 
 const app = express();
 
@@ -10,16 +10,9 @@ app.use(express.json());
 app.use(cors());
 app.use(router);
 
-
 // Configuração do Parse Server
 Parse.initialize(process.env.PARSE_APP_ID, process.env.PARSE_JS_KEY);
 Parse.serverURL = process.env.PARSE_SERVER_URL;
-
-
-app.get('/', (req, res) => {
-    res.send('Aplicação rodando com sucesso!');
-});
-
 
 console.log("Parse Server configurado com sucesso!");
 
