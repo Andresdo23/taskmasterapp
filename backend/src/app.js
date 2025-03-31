1ef72c003fbc2a64451b6841c2586473a88c963f
Andre Oliveira <andresdo026@gmail.com>
2025-03-31 01:21:49 -0300

    atualizando o backend

--- backend/src/app.js
+++ backend/src/app.js
@@ -10,23 +10,20 @@ app.use(express.json());
 app.use(cors());
 app.use(router);
 
-
 // Configuração do Parse Server
 Parse.initialize(process.env.PARSE_APP_ID, process.env.PARSE_JS_KEY);
 Parse.serverURL = process.env.PARSE_SERVER_URL;
 
+// Exemplo de rota para acessar dados
 app.get('/api/data', async (req, res) => {
-    try {
-      const query = new Parse.Query('Tasks');
-      const results = await query.find();
-      res.json(results);
-    } catch (error) {
-      res.status(500).json({ error: 'Erro ao acessar o banco de dados' });
-    }
-  });
-  
-  module.exports = app;
-
+  try {
+    const query = new Parse.Query('Tasks');
+    const results = await query.find();
+    res.json(results);
+  } catch (error) {
+    res.status(500).json({ error: 'Erro ao acessar o banco de dados' });
+  }
+});
 
 console.log("Parse Server configurado com sucesso!");
 
